ed7c06a3d4e8ae7a68812919eed89388bcababf8
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-26 11:40:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,40 @@
-'use client'
+// /app/page.js
+'use client';
 import React, { useState, useEffect } from 'react';
 import CoinCard from './components/CoinCard';
+import './styles.css';
 
 function Home() {
     const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        // Fetch the top 20 cryptocurrencies
+        async function fetchCoins() {
+            try {
+                const response = await fetch('https://api.coinlore.net/api/tickers/');
+                const data = await response.json();
+                if (response.ok) {
+                    setCoins(data.slice(0, 20)); // Get the top 20 coins
+                } else {
+                    console.error('Failed to fetch coins');
+                }
+            } catch (error) {
+                console.error('Error fetching coins:', error);
+            }
+        }
+        fetchCoins();
+    }, []);
 
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard key={coin.id} coin={coin} />
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
